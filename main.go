28da83755fa8103b9bb49e3a8d8285d24be62f8d
Matias Pan <matipan@hey.com>
2024-01-25 10:50:12 -0300

    update module

--- main.go
+++ main.go
@@ -1,22 +1,8 @@
 package main
 
-import (
-	"context"
-)
-
 type Hooks struct{}
 
 // example usage: "dagger call container-echo --string-arg yo"
 func (m *Hooks) ContainerEcho(stringArg string) *Container {
 	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
-}
-
-// example usage: "dagger call grep-dir --directory-arg . --pattern GrepDir"
-func (m *Hooks) GrepDir(ctx context.Context, directoryArg *Directory, pattern string) (string, error) {
-	return dag.Container().
-		From("alpine:latest").
-		WithMountedDirectory("/mnt", directoryArg).
-		WithWorkdir("/mnt").
-		WithExec([]string{"grep", "-R", pattern, "."}).
-		Stdout(ctx)
 }
